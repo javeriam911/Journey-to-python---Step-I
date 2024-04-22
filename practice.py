6379d788a02949aca59ecc98d2973ff55384df02
javeriam911 <javeriam911@gmail.com>
2024-04-22 15:18:51 +0500

    Python-1

---
+++ practice.py
@@ -0,0 +1,39 @@
+# print("Let's practice :1")
+
+a = int(input("enter a number : " ))
+b = int(input("enter a number  :" ))
+# print (a+b)
+# print( int(input("number :")) + int(input("number")))
+
+# print("Let's practice :2")
+
+# side = int(input("enter a side of a square :"))
+# area = side * side
+# print (area , " is the area of a square")
+
+# print("area of a square is :" , int(input("enter side of a square : "))**2)
+
+
+# print("Let's practice :3")
+
+# print((float(input("num1 :")) +float(input("num2 :")) )/2)
+
+# float1 = float(input("val1 :"))
+# float2 = float(input("val2 :"))
+# avg = (float1 + float2)/2
+# print("average of 2 floating values is " , avg)
+print("Let's practice :4")
+ 
+
+print(a >b)
+print(a <b)
+
+
+
+
+
+
+
+
+
+
---
+++ python1.py
@@ -0,0 +1,164 @@
+# print("hello world")
+# #variables
+
+# name = "javeria"
+# age = 21
+# marks = 100
+# print("my name is :" , name)
+# print("my age is :" , age)
+# print("my marks are :" , marks)
+# age2 =age
+
+# print(age2)
+#identifiers(valid or invalid variables)
+
+# VALID Variable syntax
+# myname = "javeria"
+# print(myname)
+# myName = "jiya"
+# print(myName)
+# MYNAME = "javeria mateen"
+# print(MYNAME)
+# MyName = "Javeria Mateen"
+# print(MyName)
+
+# num1 = 45
+# print(num1)
+
+##INVALID Variable syntax
+## 1num = 45 print (1num)
+
+# #num@ = "@gmail.com"
+# #print(num@)
+## @num = "Aemail.cm"
+## print(@num)
+
+
+#  Data types:
+
+# integer = 2
+# Integer = -2
+# INTEGER = 0
+# Name = "jiya"
+# Float = 0.2
+# fact = True
+# Fact = False
+# example = None
+# print(type(integer))
+# print(type(INTEGER))
+# print(type(Integer))
+# print(type(Name))
+# print(type(Float))
+# print(type(fact))
+# print(type(Fact))
+# print(type(example))
+
+
+# str1 = 'name'
+# Str1 = "name"
+# STR1 = '''name'''
+# print(str1 , Str1 , STR1)
+##program 
+
+## Print Sum
+
+# a = int (input ("enter a :"))
+# b = int(input ("enter b:"))
+# sum = a+b
+# print (sum)
+
+## COMENTS 
+# use for single line coment
+""" use for multi line coment  """ 
+"""
+print ("coment
+)
+"""
+
+### Operators amd its types
+## arithmatic operators
+# varA = int(input ("entetr a :"))
+# varB = int(input ("entetr b :"))
+
+# print( varA + varB)
+# print( varA - varB)
+# print( varA * varB)
+# print( varA /varB ," always returns a floating value")
+# print( varA **varB)
+# print( varA % varB)
+
+
+
+## Relational  OR comparision operators :
+
+# new_var = 34
+# New_var  =  43
+# print("Relational operator always returns True or False")
+# print( new_var == New_var)
+# print( new_var != New_var)
+# print( new_var <= New_var)
+# print( new_var >= New_var)
+# print( new_var > New_var)
+# print( new_var < New_var)
+
+## assignment operators :
+
+# number = 12
+# print(number)
+# number += 1
+# print(number)
+# number -= 1
+# print(number)
+# number *= 2
+# print(number)
+# number /= 3
+# print(number)
+# number **= 4
+# print(number)
+# number %= 2
+# print(number)
+
+
+##Logical operatoars :
+# print("logical operator always returns boolean")
+
+ ##not ,AND and or :
+ ##not
+# val1 = 2
+# val2 = 4
+# print(not val1>val2)
+# print(not val1<val2)
+#and
+
+
+# first = True
+# second = False
+# print(first and second)
+
+## or :
+
+#print(first or second)
+
+## type conversion or type casting :
+
+##type conversion  :
+# print("type conversion : interpretor converts automatically by itself")
+# value_1 = 1
+# value_2 = 2.0
+# print(value_1 + value_2 , "converted automatically from integer to float")
+## type casting
+# value_3 = int("5")
+# print( value_2 + value_3 , "converted manually by using int() , converted string to interger first then to floating value")
+
+
+
+## input
+
+# input_ = input("enter your name ")
+# print(input_)
+# print("always consider a string ")
+# input1 = int(input("enter a number :"))
+# print(input1)
+
+# input2 = float(input("enter a number "))
+# print(input2)
